06e86e4071e72c59749394368b4cfe946b3173a7
Anndyz <1721965764@qq.com>
2021-09-07 21:48:52 +0800

    Guava的配置和使用

---
+++ src/main/java/net/spring/online_class/config/CacheKeyManager.java
@@ -0,0 +1,11 @@
+package net.spring.online_class.config;
+
+/**
+ * Guava缓存Key管理类
+ */
+public class CacheKeyManager {
+    /**
+     * 首页轮播图缓存Key
+     */
+    public static final String INDEX_BANNER_KEY = "index:banner";
+}
--- src/main/java/net/spring/online_class/service/impl/VideoServiceImpl.java
+++ src/main/java/net/spring/online_class/service/impl/VideoServiceImpl.java
@@ -1,9 +1,11 @@
 package net.spring.online_class.service.impl;
 
+import net.spring.online_class.config.CacheKeyManager;
 import net.spring.online_class.model.entity.Video;
 import net.spring.online_class.model.entity.VideoBanner;
 import net.spring.online_class.mapper.VideoMapper;
 import net.spring.online_class.service.VideoService;
+import net.spring.online_class.utils.BaseCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,9 @@ public class VideoServiceImpl implements VideoService {
     // @Resource
     @Autowired
     private VideoMapper videoMapper;
+
+    @Autowired
+    private BaseCache baseCache;
 
     /**
      * 查询视频列表
@@ -31,7 +36,23 @@ public class VideoServiceImpl implements VideoService {
      */
     @Override
     public List<VideoBanner> listVideoBanner() {
-        return videoMapper.listVideoBanner();
+
+        try{
+            Object cacheObj =  baseCache.getTenMinuteCache().get(CacheKeyManager.INDEX_BANNER_KEY,()->{
+                List<VideoBanner> bannerList = videoMapper.listVideoBanner();
+                System.out.println("从数据库里面找轮播图列表");
+                return  bannerList;
+            });
+            // 从缓存获取到值
+            if(cacheObj instanceof List){
+                List<VideoBanner> bannerList = (List<VideoBanner>)cacheObj;
+                return bannerList;
+            }
+        }catch (Exception e){
+
+        }
+        // 缓存无值&mapper无值 return null
+        return null;
     }
 
     /**
